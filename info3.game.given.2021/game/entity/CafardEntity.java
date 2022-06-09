d0091743d976d2b166e053579d6ede264cf66365
Elise Dupont <elise.dupont@etu.univ-grenoble-alpes.fr>
2022-06-09 17:23:51 +0200

    CarfardEntity

---
+++ info3.game.given.2021/game/entity/CafardEntity.java
@@ -0,0 +1,74 @@
+package entity;
+
+import position.Direction;
+import position.PositionF;
+
+public class CafardEntity extends Entity {
+	// Item holding; Class Item TODO
+
+	CafardEntity(Scene parent, PositionF position) {
+		super(parent, position);
+	}
+
+	boolean wizz() {
+		// effet graphique
+
+	}
+
+	boolean pop() {
+		// effet graphique
+
+	}
+
+	boolean egg() {
+
+	}
+
+	boolean move(Direction direction) {
+		switch (direction) {
+		case NORD: {
+			// float x = position.getX();
+			float y = position.getY();
+			// Tile nextTile = parent.getTile(x, y - parent.getTileWidth()); // On récupère
+			// la tile où le cuisinier veut se déplacer
+			// On gère plus tard les colisions, la vérification que la tile soit libre
+			position.setY(y - parent.getTileWidth());
+			return true;
+			break;
+		}
+		case OUEST: {
+			float x = position.getX();
+			// float y = position.getY();
+			// Tile nextTile = parent.getTile(x - parent.getTileWidth(), y); // On récupère
+			// la tile où le cuisinier veut se déplacer
+			// On gère plus tard les colisions, la vérification que la tile soit libre
+			position.setX(x - parent.getTileWidth());
+			return true;
+			break;
+		}
+		case EST: {
+			float x = position.getX();
+			// float y = position.getY();
+			// Tile nextTile = parent.getTile(x + parent.getTileWidth(), y); // On récupère
+			// la tile où le cuisinier veut se déplacer
+			// On gère plus tard les colisions, la vérification que la tile soit libre
+			position.setX(x + parent.getTileWidth());
+			return true;
+			break;
+		}
+		case SUD: {
+			// float x = position.getX();
+			float y = position.getY();
+			// Tile nextTile = parent.getTile(x, y + parent.getTileWidth()); // On récupère
+			// la tile où le cuisinier veut se déplacer
+			// On gère plus tard les colisions, la vérification que la tile soit libre
+			position.setY(y + parent.getTileWidth());
+			return true;
+			break;
+		}
+		default:
+			return false;
+		}
+
+	}
+}

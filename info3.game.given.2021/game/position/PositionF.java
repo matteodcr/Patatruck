07356472ea90fde95916461c5da064f7f4a99f7a
Aurelien <aurelien.coste@etu.univ-grenoble-alpes.fr>
2022-06-09 13:03:55 +0200

    Public method for position

--- info3.game.given.2021/game/position/PositionF.java
+++ info3.game.given.2021/game/position/PositionF.java
@@ -1,27 +1,27 @@
 package position;
 
 public class PositionF {
-	float m_x;
-	float m_y;
+	private float m_x;
+	private float m_y;
 
-	PositionF(float x, float y) {
+	public PositionF(float x, float y) {
 		m_x = x;
 		m_y = y;
 	}
 
-	float getX() {
+	public float getX() {
 		return m_x;
 	}
 
-	void setX(float x) {
+	public void setX(float x) {
 		m_x = x;
 	}
 
-	float getY() {
+	public float getY() {
 		return m_y;
 	}
 
-	void setY(float y) {
+	public void setY(float y) {
 		m_y = y;
 	}
 }

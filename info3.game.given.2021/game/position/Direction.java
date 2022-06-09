2192df8692c751ad35f15cc24a321694691b0c83
Aurelien <aurelien.coste@etu.univ-grenoble-alpes.fr>
2022-06-09 14:59:55 +0200

    Update Direction => enum

--- info3.game.given.2021/game/position/Direction.java
+++ info3.game.given.2021/game/position/Direction.java
@@ -1,21 +1,5 @@
 package position;
 
-public class Direction {
-	public enum dir {
-		NORD, OUEST, EST, SUD
-	}
-
-	private dir direction;
-
-	Direction(dir direction) {
-		this.direction = direction;
-	}
-
-	void setDir(dir direction) {
-		this.direction = direction;
-	}
-
-	dir getDir() {
-		return direction;
-	}
+public enum Direction {
+	NORD, OUEST, EST, SUD;
 }

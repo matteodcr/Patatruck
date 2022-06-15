78296208307c1c667318da6d4e0463561898bd93
Mathis <mathis.grange@etu.univ-grenoble-alpes.fr>
2022-06-15 15:41:09 +0200

    Branch ready to implement entities

--- info3.game.given.2021/game/info3/game/entity/TruckEntity.java
+++ info3.game.given.2021/game/info3/game/entity/TruckEntity.java
@@ -1,5 +1,117 @@
 package info3.game.entity;
 
-public class TruckEntity {
+import info3.game.position.AutCategory;
+import info3.game.position.AutDirection;
+import info3.game.position.PositionF;
+import info3.game.scene.Scene;
+
+public class TruckEntity extends Entity {
+
+	TruckEntity(Scene parent, PositionF pos) {
+		super(parent, pos);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean pop(AutDirection direction) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean wizz(AutDirection direction) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean gwait() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean egg(AutDirection direction) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean hit(AutDirection direction) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean jump(AutDirection direction) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean explode() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean pick(AutDirection direction) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean power() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean protect(AutDirection direction) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean store() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean turn(AutDirection direction) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean gthrow(AutDirection direction) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean myDir(AutDirection direction) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean closest(AutCategory category, AutDirection direction) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean gotPower() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean gotStuff() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }

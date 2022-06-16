68f5e855b0c665cd960d3e0f8a67357a8351af1b
Edgar Onghena <dev@edgar.bzh>
2022-06-16 17:14:48 +0200

    Fix kitchen tiles overlapping with the van

--- info3.game.given.2021/game/info3/game/graphics/Sprite.java
+++ info3.game.given.2021/game/info3/game/graphics/Sprite.java
@@ -7,7 +7,7 @@ import static info3.game.graphics.Sprite.Spritesheet.COOK;
 import static info3.game.graphics.Sprite.Spritesheet.CUTTILE;
 import static info3.game.graphics.Sprite.Spritesheet.DELIVERYTILE;
 import static info3.game.graphics.Sprite.Spritesheet.FRIETILE;
-import static info3.game.graphics.Sprite.Spritesheet.KITCHEN_TRUCK;
+import static info3.game.graphics.Sprite.Spritesheet.KITCHEN;
 import static info3.game.graphics.Sprite.Spritesheet.PANTILE;
 import static info3.game.graphics.Sprite.Spritesheet.SAUCETABLETILE;
 import static info3.game.graphics.Sprite.Spritesheet.STOCKTABLE;
@@ -17,10 +17,11 @@ public enum Sprite {
 
 	PLAYER_KITCHEN(COOK, 0, 0, 1, 1), BUILDING_SQUARE_1_NORMAL(CITY, 0, 0, 1, 1),
 	BUILDING_SQUARE_1_ROUNDED(CITY, 0, 1, 1, 1), BUILDING_SQUARE_1_SQUIRCLE(CITY, 0, 2, 1, 1),
-	KITCHENTRUCK(KITCHEN_TRUCK, 0, 0, 1, 1), SAUCE_TABLE_TILE(SAUCETABLETILE, 0, 0, 1, 1),
-	FRIE_TILE(FRIETILE, 0, 0, 1, 1), CUT_TILE(CUTTILE, 0, 0, 1, 1), PAN_TILE(PANTILE, 0, 0, 1, 1),
-	TRASH_TILE(TRASHTILE, 0, 0, 1, 1), STOCK_TABLE(STOCKTABLE, 0, 0, 1, 1), BASIC_TABLE(BASICTABLE, 0, 0, 1, 1),
-	DELIVERY_TILE(DELIVERYTILE, 0, 0, 1, 1), COCKROACH_ENTITY(COCKROACH, 0, 0, 1, 1);
+	KITCHEN_TRUCK(KITCHEN, 0, 0, 15, 6), KITCHEN_TRUCK_FLOOR(KITCHEN, 1, 6, 10, 4),
+	SAUCE_TABLE_TILE(SAUCETABLETILE, 0, 0, 1, 1), FRIE_TILE(FRIETILE, 0, 0, 1, 1), CUT_TILE(CUTTILE, 0, 0, 1, 1),
+	PAN_TILE(PANTILE, 0, 0, 1, 1), TRASH_TILE(TRASHTILE, 0, 0, 1, 1), STOCK_TABLE(STOCKTABLE, 0, 0, 1, 1),
+	BASIC_TABLE(BASICTABLE, 0, 0, 1, 1), DELIVERY_TILE(DELIVERYTILE, 0, 0, 1, 1),
+	COCKROACH_ENTITY(COCKROACH, 0, 0, 1, 1);
 
 	public final Spritesheet spritesheet;
 	public final int u, v, w, h;
@@ -38,7 +39,7 @@ public enum Sprite {
 
 		BASICTABLE("BasicTableTile.png", 13), COOK("cook.png", 13), CITY("city.png", 20),
 		SAUCETABLETILE("SauceTableTile.png", 13), PANTILE("PanTile.png", 13), FRIETILE("FrieTile.png", 13),
-		CUTTILE("CutTile.png", 13), KITCHEN_TRUCK("kitchen.png", 256), TRASHTILE("TrashTile.png", 13),
+		CUTTILE("CutTile.png", 13), KITCHEN("kitchen.png", 13), TRASHTILE("TrashTile.png", 13),
 		STOCKTABLE("StockTable.png", 13), DELIVERYTILE("DeliveryTile.png", 13), COCKROACH("cockroach.png", 13),;
 
 		public final String filename;
